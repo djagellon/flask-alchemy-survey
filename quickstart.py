fb1a80e88541831e82f5b0e88ab320a3db6a4b7f
drunkmonky <drunkmonky+github@gmail.com>
2018-07-23 21:33:34 -0700

    Build json from googlesheet

--- quickstart.py
+++ quickstart.py
@@ -16,11 +16,13 @@
 """
 Shows basic usage of the Sheets API. Prints values from a Google Spreadsheet.
 """
-from __future__ import print_function
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+import json
+import pprint
 
+pp = pprint.PrettyPrinter(indent=2)
 # Setup the Sheets API
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 store = file.Storage('credentials.json')
@@ -32,18 +34,54 @@ service = build('sheets', 'v4', http=creds.authorize(Http()))
 
 # Call the Sheets API
 SPREADSHEET_ID = '1H7SdrzSY2PbW6n4bIs2ZjctXwZnN2kvU7hD7x64d1Pw'
-RANGE_NAME = 'Sheet1!A2:E'
+RANGE_NAME = 'Sheet1!C2:H'
 result = service.spreadsheets().values().get(
 	spreadsheetId=SPREADSHEET_ID,
         range=RANGE_NAME).execute()
 
 values = result.get('values', [])
+
+KEYS = ["type", "label", "condition", "title", "answers", "datalabel"]
+survey = []
+
 if not values:
     print('No data found.')
 else:
-    print('Module, Domain, Qn, Logic, Text, Answers, DataLabel:')
+    page = []
 
-    for row in values:
-        # Print columns A and E, which correspond to indices 0 and 4.
-        print('%s, %s, %s, %s, %s, %s, %s' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
-# [END sheets_quickstart]
+    for idx, row in enumerate(values):
+
+        ## page means new list
+        if len(row) == 1 and row[0] == 'page':
+            print " ===== Page Break ====="
+            survey.append(page)
+            page = []
+            continue
+
+        ## no question label means answer values belong to previous row
+        if not row[1]:
+            print "===== PREV OBJ ====="
+            question["answers"].append({
+                "text": row[4],
+                "label": row[5] 
+            })
+            continue
+
+
+        question = dict(zip(KEYS, row))
+
+        question["answers"] = [{
+            "text": row[4],
+            "label": row[5] 
+        }]
+
+        del question['datalabel']
+
+        print "APPENDING: %s" % question
+        page.append(question)
+
+    # write to file
+    file = open("surveys/test.json", "w")
+    file.write(json.dumps(survey))
+
+    pp.pprint(json.dumps(survey))
