cc39d67b2d4be8f03fb095c625f6971191f6dbda
drunkmonky <drunkmonky+github@gmail.com>
2018-09-13 11:30:00 -0700

    Replace login/auth with flask_user lib

--- app/models.py
+++ app/models.py
@@ -1,31 +1,42 @@
-from app import db, login
+from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from flask_user import UserMixin
 
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    admin = db.Column(db.Boolean)
+    email = db.Column(db.Unicode(255), nullable=False, index=True, unique=True)
+    # email_confirmed_at = db.Column(db.DateTime())
+
+    password = db.Column(db.String(255), nullable=False, server_default='')
+    active = db.Column(db.Boolean(), server_default='0')
+
     surveys = db.relationship('SurveyModel', backref='user', lazy='dynamic')
+    roles = db.relationship('Role', secondary='user_roles', backref=db.backref('user', lazy='dynamic'))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), nullable=False, server_default=u'', unique=True)  # for @roles_accepted()
+
+
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+    
 
 class SurveyModel(db.Model):
     __tablename__ = 'survey'
 
-    #TODO: link to account details
     id = db.Column(db.Integer, primary_key = True)
     module = db.Column(db.String(55))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -50,7 +61,3 @@ class QuestionModel(db.Model):
                     label = self.label,
                     answer = self.answer,
                     survey_id = self.survey_id)
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
