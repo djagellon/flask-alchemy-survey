681731a8502f62fe409f909f9ced7cb7818b8104
drunkmonky <drunkmonky+github@gmail.com>
2018-10-19 11:43:20 -0700

    short and long modules are now shown in panel expansion

--- app/static/js/report.js
+++ app/static/js/report.js
@@ -1,13 +1,5 @@
 $(function() {
 
-    async function getShortOutput(modal, label) {
-        let output_url = `/api/reports/short/${modal}/${label}`;
-        let response = await fetch(output_url);
-        let data = await response.json();
-
-        return data;
-    }
-
     async function toggleAction(module, action) {
         let complete_url = `/api/reports/complete/${module}/${action}`;
         let response = await fetch(complete_url);
@@ -33,7 +25,7 @@ $(function() {
 
     $("button.mark-complete").on("click", event => {
         let $this = $(event.currentTarget);
-        let modal = $this.parents('a')[0];
+        let modal = $this.parents('.panel')[0];
         let status = $this.data('status');
         let action = $(modal).data('answer');
         let survey_module = getModule();
@@ -48,23 +40,9 @@ $(function() {
             alert('Ooops! Something went wrong: ' + err)
         });
 
-
         event.stopPropagation();
         event.preventDefault();
 
     });
 
-    $("#shortModal").on("show.bs.modal", event => {
-        let target = $(event.relatedTarget);
-        let answer = target.data('answer');
-        let $modal_body = $(this).find('.modal-body');
-        let $more_link = $(this).find('.more-link');
-        let survey_module = getModule();
-
-        getShortOutput(survey_module, answer).then(output => {
-            $modal_body.text(output);
-            $more_link[0].href = `/report/full/${survey_module}/${answer}`;
-        });
-
-    });
 });
